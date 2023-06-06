2cd891d7b982209378e74fb70a5ff77c4ddc367b
Bahar <bahaare@hotmail.com>
2023-06-06 13:06:56 -0400

    step 1 completed:Essential HTML Elements Defined.

---
+++ script.js
@@ -0,0 +1,8 @@
+function userInput(){
+    const taskInput= document.getElementById("taskInput")
+    const addTaskButton= document.getElementById("addTaskButton")
+    let tasklist =document.getElementById("taskList")
+
+
+
+}
